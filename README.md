314ec556c0df49dca0b3c6f18af2086dcc303b4f
Brian Quinn <brianjamesquinn1@gmail.com>
2018-10-09 21:31:09 -0400

    inital readme/setup script

---
+++ README.md
@@ -0,0 +1,6 @@
+### Setting up ###
+
+1. Generate an ssh key via the command:
+  `ssh-keygen -t rsa -b 4096 -C "brianjamesquinn@gmail.com"`
+2. Add the key to the github repository
+3. Run `./environment.sh`
---
+++ environment.sh
@@ -0,0 +1,6 @@
+# Set github username and email
+git config --global user.name "Brian Quinn"
+git config --global user.email "brianjamesquinn1@gmail.com"
+
+# Download Homebrew Package Manager
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
