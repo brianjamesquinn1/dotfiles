d372f76d9e15b228b0a0762ebfd066fa2ed50481
Brian Quinn <brianjamesquinn1@gmail.com>
2020-02-18 11:18:42 -0500

    additional ctags removal :,(

--- vim.sh
+++ vim.sh
@@ -5,9 +5,6 @@ brew install vim
 # Install Vundle
 git clone git@github.com:Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# Install Ctags
-brew install --HEAD universal-ctags/universal-ctags/universal-ctags # Use this to install universal ctags (no official release version)
-
 # Command line tools used by vim plugins
 # Install Fzf
 brew install fzf
