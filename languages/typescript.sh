8d326273f37755e87099025153f9f16155b7fd27
Brian Quinn <brianjamesquinn1@gmail.com>
2025-07-20 01:58:56 -0600

    Update typescript.sh

--- languages/typescript.sh
+++ languages/typescript.sh
@@ -1,3 +1,6 @@
 # Install Typescript
+# Note that LSPs will often not attempt to start a js/ts
+# server unless node modules are present, so make sure to
+# run 'npm i' on new projects beforehand
 
 npm install -g typescript-language-server typescript
