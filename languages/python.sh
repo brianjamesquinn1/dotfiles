204fbc11d3fe610bdc7f833e6e4e1c0f766c5fa9
Brian Quinn <brianjamesquinn1@gmail.com>
2026-08-03 13:22:55 -0600

    fix python lsp

--- languages/python.sh
+++ languages/python.sh
@@ -1,6 +1,10 @@
 # Install Python
 echo "Installing Python + dependencies"
 brew install python
-brew install npm
 
-sudo npm install -g pyright
+# LSP server used by vim-lsp + ALE for Python linting
+brew install python-lsp-server
+
+# Fixers used by ALE on save
+brew install black
+brew install isort
